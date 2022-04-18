91a6bed66b89ac51a486ac5eea7cff4b9015c713
vaibhav <vaibhavsharma9526@gmail.com>
2022-04-18 17:08:24 +0530

    Add your commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const imageRandom = document.getElementById("image");
+
+function getRandomImage() {
+  const randomImageApiUrl = "https://dog.ceo/api/breeds/image/random";
+  fetch(randomImageApiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log(json);
+      var imageUrl = json.message;
+      imageRandom.src = imageUrl;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
